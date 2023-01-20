94c6e3b9d876e14e02215a6ee5aa62b885ebec35
Le Trong Khanh <trongkhanh2669@gmail.com>
2023-01-20 23:56:51 +0700

    update

--- server/index.js
+++ server/index.js
@@ -34,11 +34,31 @@ const PORT = process.env.PORT || 5000
 // Middleware
 /************************************************************/
 // app.use(express.json()).use(express.urlencoded({extended: true})).use(cors());
+app.use(cors({origin: '*'}));
+// app.use(function (req, res, next) {
+// //Enabling CORS
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//     next();
+// });
+// Add headers before the routes are defined
 app.use(function (req, res, next) {
-//Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
 });
 app.use(helmet())
@@ -47,7 +67,6 @@ app.use(bodyParser.json({limit: '100mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: "100mb", extended: true, parameterLimit:100000}));
 app.use(bodyParser.text({ limit: '500mb' }));
 
-// app.use(cors({origin: 'http://localhost:3000'}));
 
 app.use(cookies())
 app.use("/api/users", userRoute)
