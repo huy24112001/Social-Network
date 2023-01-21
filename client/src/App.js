2b2d9ff7b99df855f977bea948bbf67156519368
Le Trong Khanh <trongkhanh2669@gmail.com>
2023-01-22 02:58:46 +0700

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import Home from "./pages/home/Home";
 // import Login from "./pages/login/Login";
-import Login from "./pages/Login/index";
+import Login from "./pages/login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 
